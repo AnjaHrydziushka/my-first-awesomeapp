cd3ffbe1f154e14e127f8d857583ab8f17f95d72
AnjaHrydziushka <anja.hryd@gmail.com>
2020-05-18 11:28:52 +0200

    Deployment is ready

--- src/App.js
+++ src/App.js
@@ -6,18 +6,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>I am so freacking scared that I will never understand a word from it... But I know I will!</p>
       </header>
     </div>
   );
